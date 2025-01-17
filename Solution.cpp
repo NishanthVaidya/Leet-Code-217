658395043458eb3065109f272b1b5ab1255a6abe
NishanthVaidya <34414512+NishanthVaidya@users.noreply.github.com>
2025-01-17 06:11:28 +0530

    Delete Solution.cpp

--- Solution.cpp
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        duplicate = set()
-        for num in nums:
-            if num in duplicate:
-                return True
-            else:
-                duplicate.add(num)
-        return False
